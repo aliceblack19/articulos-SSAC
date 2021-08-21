852c6ca9965b39c45fd423e9faaffdf05d3174c0
cristhian martinez <cristhian.camargo@pi.edu.co>
2021-08-21 15:11:09 -0500

    error de codigos formularios

--- src/views/RegistroUsuarios.vue
+++ src/views/RegistroUsuarios.vue
@@ -1,21 +1,21 @@
 <template>
     <form id="app" @submit="checkForm" action="" method="post">
     <p>
-        <label for="age">Documento:<label>
+        <label for="age">Documento:</label>
         <input type="number" name="documento" id="documento" v-model="documento" min="0" required>
     </p>
     <p>
-        <label for="name">Nombres<label>
+        <label for="name">Nombres</label>
         <input type="text" name="nombres" id="nombres" v-model="nombres" required>
     </p>
 
     <p>
-        <label for="age">Correo electronico:<label>
+        <label for="age">Correo electronico:</label>
         <input type="email" name="email" id="email" v-model="email" required>
     </p>
 
     <p>
-        <label for="movie">Favorite Movie<label>
+        <label for="movie">Favorite Movie</label>
         <select name="movie" id="movie" v-model="movie">
         <option>Star Wars</option>
         <option>Vanilla Sky</option>
@@ -26,7 +26,6 @@
     <p>
     <input type="submit" value="Guardar">  
     </p>
-
 </form>
 </template>
 
